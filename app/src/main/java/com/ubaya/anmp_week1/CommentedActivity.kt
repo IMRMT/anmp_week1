a9ac75ee46bbbd238370ad11a9254dc5f53dbc38
IMRMT <michaeltimonuli@gmail.com>
2024-02-22 14:30:12 +0700

    commented activity

---
+++ app/src/main/java/com/ubaya/anmp_week1/CommentedActivity.kt
@@ -0,0 +1,14 @@
+package com.ubaya.anmp_week1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CommentedActivity : AppCompatActivity() {
+    //just to check
+    //1. Cek validity
+    //2. Cek availibility
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_commented)
+    }
+}
