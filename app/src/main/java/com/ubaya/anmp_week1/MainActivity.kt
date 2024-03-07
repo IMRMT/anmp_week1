8b2103af010fbe7bd8d4e6a73dcd845eda0a5267
IMRMT <michaeltimonuli@gmail.com>
2024-03-07 15:03:32 +0700

    week 3 - 1

--- app/src/main/java/com/ubaya/anmp_week1/MainActivity.kt
+++ app/src/main/java/com/ubaya/anmp_week1/MainActivity.kt
@@ -5,19 +5,29 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.ubaya.anmp_week1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         navController = (supportFragmentManager.findFragmentById(R.id.navHost)
                 as NavHostFragment).navController
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, binding.drawerLayout)
+
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
+        binding.bottomNav.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp()
+//        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, binding.drawerLayout) ||
+                super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/ubaya/anmp_week1/MainFragment.kt
+++ app/src/main/java/com/ubaya/anmp_week1/MainFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.navigation.Navigation
 import com.ubaya.anmp_week1.databinding.FragmentMainBinding
 
@@ -26,6 +27,10 @@ class MainFragment : Fragment() {
         binding.btnStart.setOnClickListener {
             val name = binding.txtName.text.toString()
             val action = MainFragmentDirections.actionGamefragment(name)//akan berpindah ke game fragment
+            Navigation.findNavController(it).navigate(action)
+        }
+        binding.btnSetting.setOnClickListener {
+            val action = MainFragmentDirections.actionOptionFragment()
             Navigation.findNavController(it).navigate(action)
         }
     }
